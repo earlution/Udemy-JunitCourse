4d70ed85c1b4b3f32a8b8547acf2397857d3fda0
ruari <ruari@TOSH>
2017-06-02 16:35:10 +0100

    Added first test case for StringHelper(), with inline vars.

---
+++ src/test/java/com/earlution/junitcourse/StringHelperTest.java
@@ -0,0 +1,22 @@
+package com.earlution.junitcourse;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ *
+ * @author ruari
+ */
+public class StringHelperTest {
+    
+    @Test
+    public void test() {
+        StringHelper helper = new StringHelper();
+        String actual = helper.truncateAInFirst2Positions("AACD");
+        String expected = "CD";
+        // AACD => CD, ACD => CD, CDEF => CDEF, CDAA=> CDAA
+        assertEquals(expected, actual);
+        //expected, actual
+    }
+    
+}
